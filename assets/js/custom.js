15b53c8410e96fef74cfcb46ef4783af11988a58
Erick Hernandez <erieduhergue11@gmail.com>
2019-04-02 07:32:25 -0700

    Sign up form markup.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,8 @@
+// Set a default date for datepicker
+var default_date = new Date("January 1, 2000");
 
+// Call to predefined materialize function datepicker
+$('.datepicker').datepicker({
+  defaultDate: default_date,
+  showClearBtn: true
+});
