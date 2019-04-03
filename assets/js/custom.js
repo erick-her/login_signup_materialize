dd19b7f96d07abd173b7bfd5a680e2b5e2bd8e45
Erick Hernandez <erieduhergue11@gmail.com>
2019-04-03 01:31:28 -0700

    Password match validation on sign up form.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,15 @@
-// Set a default date for datepicker
-var default_date = new Date("January 1, 2000");
+$(document).ready(function(){
+  // Set a default date for datepicker
+  var default_date = new Date("January 1, 2000");
 
-// Call to predefined materialize function datepicker
-$('.datepicker').datepicker({
-  defaultDate: default_date,
-  showClearBtn: true
+  // Call to predefined materialize function datepicker
+  $('.datepicker').datepicker({
+    defaultDate: default_date,
+    showClearBtn: true
+  });
+
+  // Validate if passwords match
+  $("input[name=password]").change(function() {
+    $("input[name=confirm_password]").attr('pattern', $(this).val());
+  });
 });
