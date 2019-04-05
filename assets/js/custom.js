c4cd3db7fcab89a831b80d7e41ce9f7f12261cc5
Erick Hernandez <erieduhergue11@gmail.com>
2019-04-05 05:10:31 -0700

    Validated signup form input fields so they aren't left empty.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-  // Set a default date for datepicker
-  var default_date = new Date("January 1, 2000");
-
-  // Call to predefined materialize function datepicker
-  $('.datepicker').datepicker({
-    defaultDate: default_date,
-    showClearBtn: true
-  });
 
   // Validate if passwords match
   $("input[name=password]").change(function() {
@@ -21,5 +13,66 @@ $(document).ready(function(){
       $("input[name=phone]").addClass('invalid');
     }
   });
-  
+
+  $('#btnSignup').click(function(){
+    // Get values of each input
+    var form = $('form');
+    var first_name = form.find('input[name="first_name"]').val().trim();
+    var last_name = form.find('input[name="last_name"]').val().trim();
+    var phone_number = form.find('input[name="phone"]').val().trim();
+    var email = form.find('input[name="email"]').val().trim();
+    var password = form.find('input[name="password"]').val().trim();
+    var confirm_password = form.find('input[name="confirm_password"]').val().trim();
+
+    var form_method = form.attr('method'); // get form method
+    var form_action = form.attr('action'); // get form action
+
+    // Validate if fields are empty
+    if(first_name == '' && last_name == '' && phone_number == '' && email == '' && password == '' && confirm_password == ''){
+      $('input').addClass('invalid');
+      return false; // so form isn't submitted
+    }
+
+    // Check to see if first name is empty
+    else if(first_name == ''){
+      $('#first_name_input input').addClass('invalid');
+      return false;
+    }
+
+    // Check to see if last name is empty
+    else if(last_name == ''){
+      $('#last_name_input input').addClass('invalid');
+      return false;
+    }
+
+    // Check to see if phone number is empty
+    else if(phone_number == ''){
+      $('#phone_number_input input').addClass('invalid');
+      return false;
+    }
+
+    // Check to see if email is empty
+    else if(email == ''){
+      $('#email_input input').addClass('invalid');
+      return false;
+    }
+
+    // Check to see if password is empty
+    else if(password == ''){
+      $('#password_input input').addClass('invalid');
+      return false;
+    }
+
+    // Check to see if confirm password is empty
+    else if(confirm_password == ''){
+      $('#confirm_password_input input').addClass('invalid');
+      return false;
+    }
+
+    else{
+
+    }
+
+  });
+
 });
